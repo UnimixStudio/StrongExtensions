0e2e9cb0f4ed4c4ca4f1648f8ef7daaf5a989f88
UnimixStudio <63611889+UnimixStudio@users.noreply.github.com>
2023-07-20 18:28:30 +0600

    Fix Bosster ApplyCoomand with Multiple Executions

---
+++ Runtime/IntExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace StrongExtensions
+{
+	public static class IntExtensions
+	{
+		public static bool InRange(this int value, int min, int max, bool includeBounds = true) =>
+			includeBounds
+				? min <= value && value <= max
+				: min < value && value < max;
+
+		public static bool Less(this int value, int other, bool include = true) =>
+			include
+				? value <= other
+				: value < other;
+
+		public static bool More(this int value, int other, bool include = true) =>
+			include
+				? value >= other
+				: value > other;
+		
+		public static bool IsZero(this int value) => value == 0;
+		public static bool IsNotZero(this int value) => value != 0;
+		public static bool IsMoreZero(this int value) => value > 0;
+		public static bool IsMoreOrEqualsZero(this int value) => value >= 0;
+	}
+}

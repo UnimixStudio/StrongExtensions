bc8294d995d0c50c6d271de5491e6de29b99f78c
UnimixStudio <63611889+UnimixStudio@users.noreply.github.com>
2023-07-20 13:20:49 +0600

    Add GameServiceContainer. Fix Booster Applying

---
+++ Runtime/ExceptionExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace StrongExtensions
+{
+	public static class ExceptionExtensions
+	{
+		public static void Try<T>(this T item, Action action, Exception defaultException = null)
+		{
+			try
+			{
+				action();
+			}
+			catch (Exception)
+			{
+				if (defaultException != null)
+					throw defaultException;
+
+				throw;
+			}
+		}
+
+		public static TResult Try<T, TResult>(this T item, Func<TResult> func, Exception defaultException = null)
+		{
+			try
+			{
+				return func();
+			}
+			catch (Exception)
+			{
+				if (defaultException != null)
+					throw defaultException;
+
+				throw;
+			}
+		}
+		public static TResult Try<T, TResult>(this T item, Func<TResult> func, out bool canExecute,Exception defaultException = null)
+		{
+			canExecute = true;
+			try
+			{
+				return func();
+			}
+			catch (Exception)
+			{
+				canExecute = false;
+				
+				if (defaultException != null)
+					throw defaultException;
+
+				throw;
+			}
+		}
+	}
+}

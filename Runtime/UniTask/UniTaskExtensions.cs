dac1d2751c8d47b88f9e211c1299cf43304e6d25
UnimixStudio <63611889+UnimixStudio@users.noreply.github.com>
2023-09-02 12:32:50 +0600

    New extensions for UniTask!!! Added new extensions for Angles to a target with and without a normal. Added new extensions for searching the plane normal of two points

---
+++ Runtime/UniTask/UniTaskExtensions.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+
+namespace StrongExtensions.UniTask
+{
+	using UniTask = Cysharp.Threading.Tasks.UniTask;
+	
+	public static class UniTaskExtensions
+	{
+		public static  UniTask WhenAll(this IEnumerable<UniTask> tasks) =>
+			UniTask.WhenAll(tasks);
+	}
+}

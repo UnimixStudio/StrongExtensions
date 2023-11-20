4a1ea668a4d75fad5f998744babe2c315520c346
UnimixStudio <63611889+UnimixStudio@users.noreply.github.com>
2023-11-20 19:20:33 +0600

    Add Timer Extensions. Reformat Code.

--- Runtime/BuilderSyntaxExtensions.cs
+++ Runtime/BuilderSyntaxExtensions.cs
@@ -4,62 +4,63 @@ using UnityEngine;
 
 namespace StrongExtensions
 {
-    public static class BuilderSyntaxExtensions
-    {
-        [HideInCallstack]
-        public static T With<T>(this T self, Action<T> action)
-        {
-            action(self);
-            return self;
-        }
+	public static class BuilderSyntaxExtensions
+	{
+		[HideInCallstack]
+		public static T With<T>(this T self, Action<T> action)
+		{
+			action(self);
+			return self;
+		}
 
-        [HideInCallstack]
-        public static T With<T>(this T self, Action<T> action, bool when)
-        {
-            if (when)
-                action(self);
-            return self;
-        }
-        [HideInCallstack]
-        public static T With<T>(this T self, Action<T> action, Func<bool> when)
-        {
-            if (when())
-                action(self);
-            return self;
-        }
+		[HideInCallstack]
+		public static T With<T>(this T self, Action<T> action, bool when)
+		{
+			if (when)
+				action(self);
+			return self;
+		}
 
-        [HideInCallstack]
-        public static T With<T>(this T self, Action<T> action, Func<T, bool> when)
-        {
-            if (when(self))
-                action(self);
-            return self;
-        }
+		[HideInCallstack]
+		public static T With<T>(this T self, Action<T> action, Func<bool> when)
+		{
+			if (when())
+				action(self);
+			return self;
+		}
 
-        [HideInCallstack]
-        public static T When<T>(this T self, Func<T, bool> when, Action action)
-        {
-            if (when(self))
-                action();
-            return self;
-        }
+		[HideInCallstack]
+		public static T With<T>(this T self, Action<T> action, Func<T, bool> when)
+		{
+			if (when(self))
+				action(self);
+			return self;
+		}
 
-        [HideInCallstack]
-        public static T When<T>(this T self, Func<T, bool> when, Action<T> action)
-        {
-            if (when(self))
-                action(self);
+		[HideInCallstack]
+		public static T When<T>(this T self, Func<T, bool> when, Action action)
+		{
+			if (when(self))
+				action();
+			return self;
+		}
 
-            return self;
-        }
+		[HideInCallstack]
+		public static T When<T>(this T self, Func<T, bool> when, Action<T> action)
+		{
+			if (when(self))
+				action(self);
 
-        [HideInCallstack]
-        public static T When<T>(this T self, bool when, Action<T> action)
-        {
-            if (when)
-                action(self);
+			return self;
+		}
 
-            return self;
-        }
-    }
+		[HideInCallstack]
+		public static T When<T>(this T self, bool when, Action<T> action)
+		{
+			if (when)
+				action(self);
+
+			return self;
+		}
+	}
 }
---
+++ Runtime/UniRx/TimerExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace UniRx.StrongExtensions
+{
+	public static class TimerExtensions
+	{
+		public static IDisposable Timer(this TimeSpan span, Action action) =>
+			Observable.Timer(span).Subscribe(action);
+	}
+}
